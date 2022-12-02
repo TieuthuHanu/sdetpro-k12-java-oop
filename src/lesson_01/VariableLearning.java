b3da886e52a66c520e1e0d66c2a592012c9913db
NHU VO <nhuvo@NHUs-Air.fios-router.home>
2022-12-01 21:30:34 -0800

    Init

---
+++ src/lesson_01/VariableLearning.java
@@ -0,0 +1,13 @@
+package lesson_01;
+
+public class VariableLearning {
+
+    public static void main(String[] args) {
+        int myIntNum = 18;
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+
+        short myShortNum = 128;
+        System.out.println(Short.MAX_VALUE);
+    }
+}
