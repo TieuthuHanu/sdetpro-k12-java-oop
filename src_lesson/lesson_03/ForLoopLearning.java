af9c45d5f61dd84f123821825e4c6f31f9dff667
NHU VO <nhuvo@NHUs-Air.fios-router.home>
2022-12-20 22:24:15 -0800

    java

---
+++ src_lesson/lesson_03/ForLoopLearning.java
@@ -0,0 +1,21 @@
+package lesson_03;
+
+public class ForLoopLearning {
+
+    public static void main(String[] args) {
+
+        int maxNum = 10;
+
+        // Infinitive loop
+        for (int i = 0; i < maxNum; ) {
+            System.out.println(maxNum);
+        }
+
+        for ( ; ; ) {
+            System.out.println(maxNum);
+        }
+
+        // Unreachable statement
+        System.out.println("Hello");
+    }
+}
