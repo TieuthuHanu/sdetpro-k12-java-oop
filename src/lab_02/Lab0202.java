e4e9c0faa8bac27ca4f06f05194ae2b043ca4940
NHU VO <nhuvo@NHUs-Air.fios-router.home>
2022-12-20 21:33:39 -0800

    Lab 02

--- src/lab_02/Lab0202.java
+++ src/lab_02/Lab0202.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Lab0202 {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner (System.in);
         System.out.print("Please enter an number: ");
         int number = scanner.nextInt();
--- src/lab_02/Lab0203.java
+++ src/lab_02/Lab0203.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Lab0203 {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Please enter your height (m) : ");
