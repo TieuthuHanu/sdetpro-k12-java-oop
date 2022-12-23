87845d663d5492c9afaf82ba0f2ace4facad5b76
NHU VO <nhuvo@NHUs-Air.fios-router.home>
2022-12-22 21:57:44 -0800

    18:37

---
+++ src_lesson/lesson_04/DoWhileLoop.java
@@ -0,0 +1,30 @@
+package lesson_04;
+
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class DoWhileLoop {
+
+    public static void main(String[] args) {
+
+        int guessingTime = 0;
+        int randomNumber = new SecureRandom().nextInt(5);
+
+        do {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Please enter your number: ");
+            int userInput = sc.nextInt();
+
+            if (userInput == randomNumber) {
+                System.out.println("You are the winner!");
+                break;
+            }
+
+            guessingTime++;
+
+        } while (guessingTime <3);
+
+        System.out.println("Random number is " + randomNumber);
+        System.out.println("See you again!");
+    }
+}
