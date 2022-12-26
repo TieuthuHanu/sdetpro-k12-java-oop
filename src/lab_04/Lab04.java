40410ad1ae23e22bfeacc7745d2203f554cef933
NHU VO <Nhu Vo>
2022-12-25 21:58:46 -0800

    Lab 04

--- src/lab_04/Lab04.java
+++ src/lab_04/Lab04.java
@@ -9,54 +9,67 @@ public class Lab04 {
 
     public static void main(String[] args) {
         printMenu();
-        int option;
-        int addTimes = new SecureRandom().nextInt(10);
-
         List<Integer> myArrayList = new ArrayList<>();
+        boolean isContinuing = true;
 
-        for (int index = 0; index < addTimes; index++) {
-            myArrayList.add(new SecureRandom().nextInt(1000));
-        }
-
-        do {
-            System.out.print("Please select an option: ");
-            Scanner sc = new Scanner(System.in);
-            option = sc.nextInt();
+        while (isContinuing) {
+            int option = getMenuOption();
 
             switch (option) {
-                case 1: System.out.println(myArrayList); break;
-                case 2: System.out.println(maxValue(myArrayList)); break;
-                case 3: System.out.println(minValue(myArrayList)); break;
-                case 4: searchNumber(myArrayList); break;
+                case 1: addNumber(myArrayList); break;
+                case 2: printListOfNumbers(myArrayList); break;
+                case 3: maxValue(myArrayList); break;
+                case 4: minValue(myArrayList); break;
+                case 5: searchNumber(myArrayList); break;
+                case 0: isContinuing = false; break;
+                default: System.out.println("Wrong option!");
             }
-        } while (option == 1 || option == 2 || option == 3 || option == 4);
+        }
     }
 
     public static void printMenu() {
         System.out.println("======MENU======");
-        System.out.println("1. Print all numbers");
-        System.out.println("2. Print maximum value");
-        System.out.println("3. Print minimum value");
-        System.out.println("4. Search number");
+        System.out.println("1. Add numbers into list");
+        System.out.println("2. Print all numbers");
+        System.out.println("3. Print maximum value");
+        System.out.println("4. Print minimum value");
+        System.out.println("5. Search number");
+        System.out.println("0. Exit!");
         System.out.println("================");
     }
 
-    public static int maxValue(List<Integer> arrayList) {
+    public static int getMenuOption() {
+        System.out.print("Please select an option: ");
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
+    }
+
+    public static void addNumber(List<Integer> arrayList) {
+        int number = new SecureRandom().nextInt(1000);
+        arrayList.add(number);
+        System.out.println(number);
+    }
+
+    public static void printListOfNumbers(List<Integer> arrayList) {
+        System.out.println(arrayList);
+    }
+
+    public static void maxValue(List<Integer> arrayList) {
         int max = arrayList.get(0);
         for (int index = 1; index < arrayList.size(); index++) {
             if (arrayList.get(index) > max)
                 max = arrayList.get(index);
         }
-        return max;
+        System.out.println(max);
     }
 
-    public static int minValue(List<Integer> arrayList) {
+    public static void minValue(List<Integer> arrayList) {
         int min = arrayList.get(0);
         for (int index = 1; index < arrayList.size(); index++) {
             if (arrayList.get(index) < min)
                 min = arrayList.get(index);
         }
-        return min;
+        System.out.println(min);
     }
 
     public static void searchNumber(List<Integer> arrayList) {
